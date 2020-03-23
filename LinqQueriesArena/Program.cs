3a15b4d5167c95d3f4b9318c5636cd302736cf8e
Architrana <archit11103443@gmail.com>
2020-03-23 09:25:30 +0530

    Data table from CSV format String

--- LinqQueriesArena/Program.cs
+++ LinqQueriesArena/Program.cs
@@ -10,6 +10,32 @@ namespace LinqQueriesArena
     {
         static void Main(string[] args)
         {
+            string str = @"a,b,c,d" + System.Environment.NewLine + "1,2,3,4";
+            string[] arrStr = str.Split('\n');
+                bool isColumnSet = false;
+            System.Data.DataTable dtConnectionTable = new System.Data.DataTable();
+            foreach (string item in arrStr)
+            {
+                if (!isColumnSet)
+                {
+                    foreach (string j in item.Split(',')) { dtConnectionTable.Columns.Add(j); }
+                    isColumnSet = true;
+                }
+                else {
+                    System.Data.DataRow dr = dtConnectionTable.NewRow();
+                    dr.ItemArray = item.Split(',');
+                    dtConnectionTable.Rows.Add(dr);
+                }
+
+            }
+
+            foreach (System.Data.DataRow item in dtConnectionTable.Rows)
+            {
+                string strRow = "";
+                foreach (string j in item.ItemArray) { strRow = strRow + ","+ j ; }
+                Console.WriteLine(strRow);
+            }
+            Console.Read();
         }
     }
 }
